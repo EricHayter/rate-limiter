8331cf3670d07b65c745a9eddd01924afb3e693a
Eric Hayter <hayter.eric@gmail.com>
2023-06-14 15:54:44 -0400

    fixing bugs from refactor

--- rate_limiter.py
+++ rate_limiter.py
@@ -4,26 +4,21 @@ from units import Units
 
 
 '''
-- logfile for errors (prob not)
-- calculate time needs to be finished
-- clean up (think the dictionaries in functions)
-- BIG PROBLEM XD:
-    usage isn't resetting correctly
-    run a sim after cd is issued only 1 request is made
-
-
 - ALMOST the right behaviour. for some reason we are allowd to make 1 additional
   request after the first set of cooldowns
 
-- make parse_usage function?
 - efficiency in reset usage
 - init
 - update_limits
 - write usage
 
+- calculate time isn't right atm
 - what are we doing with optional arguments?
 - package all the files together into one module?
 - or just move enum class to this module?
+- usage isn't storing at all now
+- rename parse_unit to something better
+- writing to files isn't quite working yet
 '''
 
 class RateLimiter:
@@ -51,31 +46,40 @@ class RateLimiter:
         else:
             self.latest_time = datetime.fromisoformat(self.config['USAGE']['latest_time'])
             for key, value in self.config['USAGE'].items():
-                if key in dir(Units) and value.isdigit():
-                    self.usage[key] = int(value)
+                unit = RateLimiter.parse_unit(key)
+                if unit != None and value.isdigit():
+                    self.usage[unit] = int(value)
 
+    # not sure about this being in the class
     def parse_limits(self):
         for param in self.config['LIMITS']:
-            if param == 'year':
-                self.usage[Units.YEAR] = 0
-            elif param == 'month':
-                salf.usage[Units.MONTH] = 0
-            elif param == 'day':
-                self.usage[Units.DAY] = 0
-            elif param == 'hour':
-                self.usage[Units.HOUR] = 0
-            elif param == 'minute':
-                self.usage[Units.MINUTE] = 0
-            elif param == 'second':
-                self.usage[Units.SECOND] = 0
+            if (unit := parse_unit) != None:
+                self.usage[unit] = 0
             else:
                 raise Exception(f'unknown time unit in LIMITS: {param}')
 
 
+
+    def parse_unit(string: str) -> Units:
+        if string == 'year':
+            return Units.YEAR
+        elif string == 'month':
+            return Units.MONTH
+        elif string == 'day':
+            return Units.DAY
+        elif string == 'hour':
+            return Units.HOUR
+        elif string == 'minute':
+            return Units.MINUTE
+        elif string == 'second':
+            return Units.SECOND
+        else:
+            return None
+
+
     def update_limits(self, **kwargs):
-        #cfg_file = kwargs['cfg_file'] if 'cfg_file' in kwargs else 'limits.cfg'
         for key, value in kwargs.items():
-            if key in dir(Units) and value.isdigit():
+            if RateLimiter.parse_unit(key) != None and value.isdigit():
                 self.config['LIMITS'][key] = value
         with open(self.cfg_file, 'w') as configfile:
             self.config.write(configfile)
@@ -94,15 +98,13 @@ class RateLimiter:
         returns the amount of seconds until you can make another request
         '''
         for unit in self.config['LIMITS']:
-            # this isn't a safe operation for casting
-            if self.usage[unit] >= int(self.config['LIMITS'][unit]):
+            # something seems to be wrong with the if statement here
+            u = RateLimiter.parse_unit(unit)
+            if u != None and self.usage[u] >= int(self.config['LIMITS'][unit]):
                 return self.calculate_time(unit)
         
         return 0
 
-        # they are in the same time period and have credits to use
-        # right time period but over limit
-        # need to reset time
 
     def is_after(self, unit: Units) -> bool:
         '''
@@ -119,12 +121,14 @@ class RateLimiter:
         else:
             if current_time[i+1] == latest_time[i+1]:
                 return False
-
         return True
+
 
     def reset_usage(self, unit: Units):
         # not 100% efficient as usage would be storing all params
         for u in reversed(Units):
+            if u not in self.usage:
+                continue
             self.usage[u] = 0
             if u == unit:
                 break
@@ -132,9 +136,9 @@ class RateLimiter:
 
     def calculate_time(self, unit: Units):
         next_increment = [*datetime.min.timetuple()][:6]
-        for u in range(Units):
-            next_unit[u.value] = self.latest_time.timetuple()[u.value]
-        next_increment = datetime(*next_unit)
+        for u in Units:
+            next_increment[u.value] = self.latest_time.timetuple()[u.value]
+        next_increment = datetime(*next_increment)
 
         if unit == Units.YEAR:
             next_increment += timedelta(years=1)
@@ -146,7 +150,7 @@ class RateLimiter:
             next_increment += timedelta(hours=1)
         elif unit == Units.MINUTE:
             next_increment += timedelta(minutes=1)
-        elif unit == Unit.SECOND:
+        elif unit == Units.SECOND:
             next_increment += timedelta(seconds=1)
 
         return (next_increment - self.latest_time).total_seconds()
@@ -155,7 +159,7 @@ class RateLimiter:
     def request(self, func):
         ''' returning the value'''
         try:
-            func()
+            output = func()
             for unit in self.usage:
                 self.usage[unit] += 1
                 if self.is_after(unit):
@@ -165,4 +169,6 @@ class RateLimiter:
         except Exception as e:
             self.write_usage()
             raise e
+
+        return output
 
--- test_rate_limiter.py
+++ test_rate_limiter.py
@@ -6,23 +6,19 @@ def foo():
     return
 
 def main():
-    for i in Units:
-        print(i)
-        print(i.value + 1)
+    counter = 0
+    rl = rate_limiter.RateLimiter(cfg='./limits.cfg')
 
-#    counter = 0
-#    rl = rate_limiter.RateLimiter(cfg='./limits.cfg')
-#
-#    start = time.time()
-#    while counter < 1000:
-#        if (cd := rl.request_cooldown()) > 0:
-#            print(f'sleeping for {cd} seconds')
-#            time.sleep(cd)
-#        rl.request(foo)
-#        print(f'counter: {counter}')
-#        counter += 1
-#
-#    rl.write_usage()
+    start = time.time()
+    while counter < 1000:
+        if (cd := rl.request_cooldown()) > 0:
+            print(f'sleeping for {cd} seconds')
+            time.sleep(cd)
+        rl.request(foo)
+        print(f'counter: {counter}')
+        counter += 1
+
+    rl.write_usage()
 
 
 
